fc947ad72e5f7df203a3b5a69151f2cf57c3aab1
Hong-Ye Hu <hongyehu.physics@gmail.com>
2024-04-26 21:25:18 -0400

    update optimization

--- simple_Ham/optimization.jl
+++ simple_Ham/optimization.jl
@@ -7,45 +7,127 @@ using Optimization, OptimizationNOMAD
 using ProgressMeter
 include("Hamiltonian.jl")
 
-# function rosenbrock(u, p)
-#     return (p[1] - u[1])^2 + p[2] * (u[2] - u[1]^2)^2
-# end
-# u0 = zeros(2)
-# p = [1.0, 100.0]
-# prob = OptimizationProblem(rosenbrock, u0, p, lb = [-1.0, -1.0], ub = [1.0, 1.0])
-# sol = Optimization.solve(prob, NOMADOpt())
-# print(sol)
 
 """
 x = [J,U,t,δ1up,δ1down,δ2up,δ2down,θ]
 """
 function objective(x,p)
-    target = target_op(1)+target_op(1)';
+    target = target_op(p[1])+target_op(p[1])';
     U = Uevolve(
-    x[1],x[2],1.0,x[3],x[4],x[5],x[6],x[7],1
+    x[1],x[2],1.0,x[3],x[4],x[5],x[6],x[7],p[1]
     );
     transformed = U*target*U';
     residual = transformed - Diagonal(diag(transformed));
     return sum(abs.(residual))
 end
-function transformed_obs(x)
-    target = target_op(1)+target_op(1)';
+function transformed_obs(x,p)
+    target = target_op(p[1])+target_op(p[1])';
     U = Uevolve(
-    x[1],x[2],1.0,x[3],x[4],x[5],x[6],x[7],1
+    x[1],x[2],1.0,x[3],x[4],x[5],x[6],x[7],p[1]
     );
     transformed = U*target*U';
     return transformed
 end
-x = [1.0,2.0,1.0,3.9,4.0,5.0,6.0]
-objective(x,0)
-x0 = rand(7)
-prob = OptimizationProblem(objective, x0)
-sol = Optimization.solve(prob, NOMADOpt())
-sol.minimum
-sol.u
-transformed_obs(sol.u)
+"""
+x = [J,U,t,δ1up,δ1down,δ2up,δ2down,θ]
+"""
+function constraint_objective(x,p)
+    target = target_op(p[1])+target_op(p[1])';
+    U = Uevolve(
+    x[1],x[2],1.0,0.0,0.0,0.0,0.0,x[3],p[1]
+    );
+    transformed = U*target*U';
+    residual = transformed - Diagonal(diag(transformed));
+    return sum(abs.(residual))
+end
+function joint_constraint_objective(x,p)
+    target1 = target_op(1)+target_op(1)';
+    U1 = Uevolve(
+    x[1],x[2],1.0,0.0,0.0,0.0,0.0,x[3],1
+    );
+    transformed1 = U1*target1*U1';
+    residual1 = transformed1 - Diagonal(diag(transformed1));
+    target2 = target_op(2)+target_op(2)';
+    U2 = Uevolve(
+    x[1],x[2],1.0,0.0,0.0,0.0,0.0,x[3],2
+    );
+    transformed2 = U2*target2*U2';
+    residual2 = transformed2 - Diagonal(diag(transformed2));
+    return sum(abs.(residual1))+sum(abs.(residual2))
+end
+function constrait_transformed_obs(x,p)
+    target = target_op(p[1])+target_op(p[1])';
+    U = Uevolve(
+    x[1],x[2],1.0,0.0,0.0,0.0,0.0,x[3],p[1]
+    );
+    transformed = U*target*U';
+    return transformed
+end
+function residual_constrait_transformed_obs(x,p)
+    target = target_op(p[1])+target_op(p[1])';
+    U = Uevolve(
+    x[1],x[2],1.0,0.0,0.0,0.0,0.0,x[3],p[1]
+    );
+    transformed = U*target*U';
+    residual = transformed - Diagonal(diag(transformed));
 
-x0[2] = 1.2
-x0
-bounded_prob = OptimizationProblem(objective, x0,lb = [0.0, 1.0,-10.0,-10.0,-10.0,-10.0,0.0], ub = [10.0, 2.0,10.0,10.0,10.0,10.0,π*2])
+    return U'*residual*U
+end
+
+
+x0 = rand(7)
+x0[1]=x0[1]+1
+x0[2]=x0[2] 
+p=[2]
+bounded_prob = OptimizationProblem(objective, x0,p,lb = [1.0, 0.3,-10.0,-10.0,-10.0,-10.0,0.0], ub = [10.0, 5.0,10.0,10.0,10.0,10.0,π*2])
 bounded_sol = Optimization.solve(bounded_prob, NOMADOpt())
+bounded_sol.minimum
+bounded_sol.u
+
+
+
+constraint_x0 = rand(3)
+constraint_x0[1] += constraint_x0[1]+1
+constraint_x0[2] += constraint_x0[2]+0.1
+constraint_x0[3]=0.005
+p = [2]
+constraint_bounded_prob = OptimizationProblem(constraint_objective, constraint_x0,p,lb = [1.0, 0.1,0.0], ub = [10.0, 2.0,0.01])
+constraint_bounded_sol = Optimization.solve(constraint_bounded_prob, NOMADOpt())
+constraint_bounded_sol.minimum
+constraint_bounded_sol.u
+
+constrait_transformed_obs(constraint_bounded_sol.u,p)
+
+
+constraint_x0 = rand(3)
+constraint_x0[1] += constraint_x0[1]+3
+constraint_x0[2] += constraint_x0[2]+0.1
+p = []
+joint_constraint_bounded_prob = OptimizationProblem(joint_constraint_objective, constraint_x0,p,lb = [1.0, 0.1,0.0], ub = [5.0, 2.0,2*π])
+joint_constraint_bounded_sol = Optimization.solve(joint_constraint_bounded_prob, NOMADOpt())
+joint_constraint_bounded_sol.minimum
+joint_constraint_bounded_sol.u
+x_test = [4.32,0.1,0.7854]
+round.(constrait_transformed_obs(x_test,[1]),digits=3)
+round.(constrait_transformed_obs(x_test,[2]),digits=3)
+round.(residual_constrait_transformed_obs(joint_constraint_bounded_sol.u,[1]),digits=3)
+round.(residual_constrait_transformed_obs(joint_constraint_bounded_sol.u,[2]),digits=3)
+target_op(2)+target_op(2)'
+
+
+
+o = Op_fixed(4,2);
+basis(o)
+ada(o,4,3)+ada(o,3,4)+ada(o,2,1)+ada(o,1,2)
+
+
+
+
+us, vs = eigen(Matrix(target_op(2)+target_op(2)'))
+v1 = [0;0;0;0;1/sqrt(2);0].+[0;1/sqrt(2);0;0;0;0]
+o = Matrix(target_op(2)+target_op(2)')
+o*v1
+vs[:,1]
+2*vs[:,1]*vs[:,1]'-2*vs[:,end]*vs[:,end]'
+target_op(3)+target_op(3)'
+ϕ1 = 0.5*([])
---
+++ test/free_fermion_test.jl
@@ -0,0 +1,90 @@
+using Fermionic
+function Diag_real_skew(M, rand_perturbation::Int64=0)
+    N = div(size(M, 1), 2)
+  
+    # Random perturbation before forcing skew symmetrisation
+    if (rand_perturbation != 0)
+      if (rand_perturbation == 1)
+          random_M = rand(2N, 2N) * eps()
+          random_M = (random_M - random_M') / 2.0
+          M += random_M
+        end
+        if (rand_perturbation == 4)
+          random_M = zeros(Complex{Float64}, 2N, 2N)
+          random_M[1, N+2] = eps()
+          random_M[2, N+1] = -random_M[1, N+2]
+          random_M = (random_M - random_M') / 2.0
+          M += random_M
+        end
+        if (rand_perturbation == 5)
+          random_M = zeros(Complex{Float64}, 2N, 2N)
+          r = eps()
+          M[1, 2] += r
+          M[2, 1] -= r
+        end
+    end
+  
+    Schur_object = LinearAlgebra.schur(M)
+  
+    Schur_ort_i = Schur_object.vectors
+    Schur_blocks_i = Schur_object.Schur
+  
+    # Reorder so that all 2x2 blocks have positive value in top right, accounting for 1x1 blocks.
+    swap_perm = Vector{Int64}(undef, 0)
+    sizehint!(swap_perm, 2N)
+    iiter = 1
+    while iiter < 2N
+        if abs(Schur_blocks_i[iiter, iiter]) >= abs(Schur_blocks_i[iiter+1, iiter]) # alternative: s_b_i[i+1, i] == 0
+            # We have a 1x1 block - move it to the front. These always come in pairs, but not always sequentially.
+            pushfirst!(swap_perm, iiter)
+            iiter += 1
+        elseif (Schur_blocks_i[iiter + 1, iiter] >= 0.0)
+            # Flipped 2x2 block
+            push!(swap_perm, iiter + 1, iiter)
+            iiter += 2
+        else
+            # Unflipped 2x2 block
+            push!(swap_perm, iiter, iiter + 1)
+            iiter += 2
+        end
+    end
+    if iiter == 2N  # catch the final 1x1 block if it exists
+      pushfirst!(swap_perm, 2N)
+    end
+    
+    M_temp = Schur_blocks_i[swap_perm, swap_perm]
+    O_temp = Schur_ort_i[:, swap_perm]
+  
+    # Sort the blocks, λ_1>=λ_2>=...>=λ_N with λ_1 the coefficient in the upper left block
+    psort = sortperm(diag(M_temp, 1)[begin:2:end], rev=true)
+    full_psort = zeros(Int64, 2N)
+    full_psort[begin:2:end] .= 2 .* psort .- 1
+    full_psort[begin + 1:2:end] .= 2 .* psort
+  
+    M_f = M_temp[full_psort, full_psort]
+    O_f = real(O_temp[:, full_psort])
+  
+    return M_f, O_f
+  end
+
+using Random
+
+function random_antisymmetric_matrix(n::Int64)
+    # Initialize a n x n matrix with zeros
+    A = zeros(Float64, n, n)
+    
+    # Fill the upper triangular part above the diagonal with random values
+    for i in 1:n
+        for j in (i+1):n
+            A[i, j] = randn()  # Generate a random number from a normal distribution
+            A[j, i] = -A[i, j]  # Set the symmetric element to the negative
+        end
+    end
+    
+    return A
+end
+test = random_antisymmetric_matrix(200)
+M, O = Diag_real_skew(test);
+round.(M,digits=2)
+
+Cartesian2Index([1,1],[2,2],2)
